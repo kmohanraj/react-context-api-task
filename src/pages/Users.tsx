5ecaf34d691a96925b68b51bf5927f2582a6a5dc
saran <mohanksv.89@gmail.com>
2022-05-28 15:47:52 +0530

    folder structure

---
+++ src/pages/Users.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+
+const Users = () => {
+  return(
+    <Container>
+      <h1>Test</h1>
+    </Container>
+  )
+}
+
+
+export default Users;
